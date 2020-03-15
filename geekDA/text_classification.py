bf74cdae8063baa3d0d4f44d145278a53ff6b352
ybgm <cyibinkb@gmail.com>
2020-03-15 14:59:57 +0800

    朴素贝叶斯分类

--- geekDA/text_classification.py
+++ geekDA/text_classification.py
@@ -23,7 +23,7 @@ def cut_words(file_path):
 #获取所以文档分词和标签
 def loadfile(file_dir,label):
     """
-    将路径下的所有文件加载
+    将路径下的所有文件加载    
     :param file_dir: 保存txt文件目录
     :param label: 文档标签
     :return: 分词后的文档列表和标签
@@ -37,21 +37,21 @@ def loadfile(file_dir,label):
         labels_list.append(label)
     return words_list,labels_list     #Python\geekDA\text classification\test\女性
 
-train_words_list1,train_labels1 = loadfile('./Python/geekDA/text classification/train/女性','女性')
-train_words_list2,train_labels2 = loadfile('./Python/geekDA/text classification/train/体育','体育')
-train_words_list3,train_labels3 = loadfile('./Python/geekDA/text classification/train/文学','文学')
-train_words_list4,train_labels4 = loadfile('./Python/geekDA/text classification/train/校园','校园')
+train_words_list1,train_labels1 = loadfile('./geekDA/text classification/train/女性','女性')
+train_words_list2,train_labels2 = loadfile('./geekDA/text classification/train/体育','体育')
+train_words_list3,train_labels3 = loadfile('./geekDA/text classification/train/文学','文学')
+train_words_list4,train_labels4 = loadfile('./geekDA/text classification/train/校园','校园')
 train_words_list = train_words_list1 + train_words_list2 + train_words_list3 + train_words_list4
 train_labels = train_labels1 + train_labels2 + train_labels3 + train_labels4
 
-test_words_list1,test_labels1 = loadfile('./Python/geekDA/text classification/test/女性','女性')
-test_words_list2,test_labels2 = loadfile('./Python/geekDA/text classification/test/体育','体育')
-test_words_list3,test_labels3 = loadfile('./Python/geekDA/text classification/test/文学','文学')
-test_words_list4,test_labels4 = loadfile('./Python/geekDA/text classification/test/校园','校园')
+test_words_list1,test_labels1 = loadfile('./geekDA/text classification/test/女性','女性')
+test_words_list2,test_labels2 = loadfile('./geekDA/text classification/test/体育','体育')
+test_words_list3,test_labels3 = loadfile('./geekDA/text classification/test/文学','文学')
+test_words_list4,test_labels4 = loadfile('./geekDA/text classification/test/校园','校园')
 test_words_list = test_words_list1 + test_words_list2 + test_words_list3 + test_words_list4
 test_labels = test_labels1 + test_labels2 + test_labels3 + test_labels4
 # Python\geekDA\text classification\stop
-stop_words = open ('./Python/geekDA/text classification/stop/stopword.txt','r',encoding='utf-8').read()
+stop_words = open ('./geekDA/text classification/stop/stopword.txt','r',encoding='utf-8').read()
 stop_words = stop_words.encode('utf-8').decode('utf-8-sig')# 列表头部\ufeff处理 ；uft-8-sig"中sig全拼为 signature 也就是"带有签名的utf-8
 stop_words = stop_words.split('\n') # 根据分隔符分隔
 #计算单词权重
